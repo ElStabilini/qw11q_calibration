2ef5e7d0d59d7094466fc4c13025f5242309d631
ElStabilini <el.stabilini@gmail.com>
2024-09-18 09:07:27 +0200

    bash script upload

---
+++ runcard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# SBATCH directives (optional)
+#SBATCH --job-name=resonator_high_power  # Job name
+#SBATCH --time=01:00:00            # Time limit
+#SBATCH --partition=qw11q            # Partition name
+
+# Run the qq acquire command
+qq auto /home/users/elisa.stabilini/cal_qw11q/resonator_spectroscopy_high.yaml -o /home/users/elisa.stabilini/cal_qw11q/output/resonator_spectroscopy_high
