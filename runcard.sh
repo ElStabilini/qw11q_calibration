c6d22011c309c4b1aaeed86422555e55bd435466
ElStabilini <el.stabilini@gmail.com>
2024-09-19 11:13:31 +0400

    validation

--- runcard.sh
+++ runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=readout_optimization  # Job name
+#SBATCH --job-name=validation  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/readout_optimization.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D1/readout_optimization
+qq auto /home/users/elisa.stabilini/cal_qw11q/validation.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D1/validation
