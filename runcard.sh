e7f1879a5dfc00462b0770c14cfe2c00e19ce9d9
ElStabilini <el.stabilini@gmail.com>
2024-09-19 10:39:27 +0400

    RX fine tuning

--- runcard.sh
+++ runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=resonator_high_power  # Job name
+#SBATCH --job-name=readout optimization  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/classification.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D1/classification
+qq auto /home/users/elisa.stabilini/cal_qw11q/readout_optimization.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D1/readout_optimization
