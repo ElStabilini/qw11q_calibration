5370dfa94767e1a35852c0097ea5820800751f1c
ElStabilini <el.stabilini@gmail.com>
2024-09-19 12:01:46 +0400

    recalibration resonator high power

--- runcard.sh
+++ runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=validation  # Job name
+#SBATCH --job-name=res_spectroscopy_high  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/rb.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D1/randomized_benchmarking
+qq auto /home/users/elisa.stabilini/cal_qw11q/resonator_spectroscopy_high.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D2/resonator_spectroscopy_high_1
