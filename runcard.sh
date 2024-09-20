c2e7784bacf1ee20630c68e3ab2e7e54198008fe
ElStabilini <el.stabilini@gmail.com>
2024-09-20 13:16:13 +0400

    resonator punchout

--- runcard.sh
+++ runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=resonator_spectroscopy  # Job name
+#SBATCH --job-name=resonator_punchout  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/resonator_characterization.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D2/resonator_punchout
+qq auto /home/users/elisa.stabilini/cal_qw11q/resonator_characterization.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D2/resonator_punchout_1
