17b8b2f7169f0121e59c06cc5713d05cf3f932aa
ElStabilini <el.stabilini@gmail.com>
2024-09-20 14:09:36 +0400

    resonator punchout D3

--- calibrationD3.sh
+++ calibrationD3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=resonator_spectroscopy  # Job name
+#SBATCH --job-name=resonator_punchout  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/resonator_spectroscopy_high.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D3/resonator_spectroscopy_high
+qq auto /home/users/elisa.stabilini/cal_qw11q/resonator_punchout.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D3/resonator_punchout
