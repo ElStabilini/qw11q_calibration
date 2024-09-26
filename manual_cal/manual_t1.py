40bb741ea2574c4d4fac0bed4cb28666c820d461
ElStabilini <el.stabilini@gmail.com>
2024-09-26 11:28:01 +0400

    manual calibration test

--- manual_cal/manual_t1.py
+++ manual_cal/manual_t1.py
@@ -15,22 +15,22 @@ executor = Executor.create(
   #output=pathlib.Path("experiment_data")
 )
 
-t1_params = {
-    "id": "t1_experiment",
-    "targets": [0],  # we are defining here which qubits to analyze
-    "operation": "t1_signal",
-    "parameters": {
-        "delay_before_readout_start": 0,
-        "delay_before_readout_end": 20_000,
-        "delay_before_readout_step": 50,
-    },
-}
+#t1_params = {
+    #"id": "t1_experiment",
+   # "targets": [0],  # we are defining here which qubits to analyze
+   # "operation": "t1_signal",
+   # "parameters": {
+       # "delay_before_readout_start": 0,
+       # "delay_before_readout_end": 20_000,
+       # "delay_before_readout_step": 50,
+   # },
+#}
 
-executor.run_protocol(t1_signal, t1_params, ExecutionMode.ACQUIRE)
-executor.run_protocol(t1_signal, t1_params, ExecutionMode.FIT)
+#executor.run_protocol(t1_signal, t1_params, ExecutionMode.ACQUIRE)
+#executor.run_protocol(t1_signal, t1_params, ExecutionMode.FIT)
 
-history = executor.history
-t1_res = history["t1_experiment"]  # id of the protocol
+#history = executor.history
+#t1_res = history["t1_experiment"]  # id of the protocol
 
-data = t1_res.data  # raw data
-results = t1_res.results  # fit data
+#data = t1_res.data  # raw data
+#results = t1_res.results  # fit data
