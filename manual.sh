ac09e1498e57751b5449c1100b48d38343bbc927
ElStabilini <el.stabilini@gmail.com>
2024-09-26 09:37:42 +0200

    removed duplicate

--- manual.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH directives (optional)
-#SBATCH --job-name=test  # Job name
-#SBATCH --time=01:00:00            # Time limit
-#SBATCH --partition=dummy            # Partition name
-
-#run the script
-python manual_t1.py
