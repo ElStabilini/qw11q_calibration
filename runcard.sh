d563d87e67b584b6d6d298a8f8af3ae40bcde019
ElStabilini <el.stabilini@gmail.com>
2024-10-01 09:10:43 +0400

    classification qibocal standard

--- runcard.sh
+++ runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/resonator_spectroscopy_high.yaml -o /home/users/elisa.stabilini/cal_qw11q/output_D2_2/resonator_spectroscopy_high
+qq auto /home/users/elisa.stabilini/cal_qw11q/classification.yaml -o /home/users/elisa.stabilini/cal_qw11q/manual_cal/D2_mask/classification_1
