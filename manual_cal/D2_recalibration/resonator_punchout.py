76e4537b8df8ba15ccd6df867cc52bac941d581f
ElStabilini <el.stabilini@gmail.com>
2024-10-02 13:06:15 +0400

    resonator punchout changed central amplitude

--- manual_cal/D2_recalibration/resonator_punchout.py
+++ manual_cal/D2_recalibration/resonator_punchout.py
@@ -4,7 +4,7 @@ from qibocal.cli.report import report
 target = "D2" 
 with Executor.open(
     "myexec",
-    path="resonator_punchout",
+    path="resonator_punchout_4",
     platform="qw11q",
     targets=[target],
     update=True,
@@ -13,13 +13,13 @@ with Executor.open(
     e.platform.settings.nshots = 2048
 
     resonator_punchout = e.resonator_punchout(
-        amplitude = 0.05,
-        freq_step = 2000000,
+        amplitude = 0.1,
+        freq_step = 200000,
         freq_width = 40000000,
         max_amp_factor = 2,
         min_amp_factor = 0,
         relaxation_time = 5000,
-        step_amp_factor = 0.1,    
+        step_amp_factor = 0.01,    
 )
 
 report(e.path, e.history)
