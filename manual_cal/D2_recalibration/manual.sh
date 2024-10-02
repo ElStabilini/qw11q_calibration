f2c1d7f6bd592ec8499ab15732de4e9659289417
ElStabilini <el.stabilini@gmail.com>
2024-10-02 11:53:11 +0400

    resonator punchout

--- manual_cal/D2_recalibration/manual.sh
+++ manual_cal/D2_recalibration/manual.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=manual_res  # Job name
-#SBATCH --time=01:00:00            # Time limit
+#SBATCH --time=00:30:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 #run the script
-python resonator_spectroscopy_low.py
+python resonator_punchout.py
