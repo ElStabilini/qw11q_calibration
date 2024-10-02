2070877332cf1d31710d4acdf8b266c2b056b92a
ElStabilini <el.stabilini@gmail.com>
2024-10-02 13:34:11 +0400

    resonator punchout

--- manual_cal/D2_recalibration/qubit_spectroscopy.py
+++ manual_cal/D2_recalibration/qubit_spectroscopy.py
@@ -4,7 +4,7 @@ from qibocal.cli.report import report
 target = "D2" 
 with Executor.open(
     "myexec",
-    path="qubit_spectroscopy_22",
+    path="qubit_spectroscopy",
     platform="qw11q",
     targets=[target],
     update=True,
@@ -16,7 +16,7 @@ with Executor.open(
         drive_amplitude = 0.006,
         drive_duration = 4000,
         freq_step = 100000,
-        freq_width = 50000000,
+        freq_width = 35000000,
         relaxation_time = 5000,
     )
 
--- manual_cal/D2_recalibration/resonator_punchout.py
+++ manual_cal/D2_recalibration/resonator_punchout.py
@@ -4,7 +4,7 @@ from qibocal.cli.report import report
 target = "D2" 
 with Executor.open(
     "myexec",
-    path="resonator_punchout_4",
+    path="resonator_punchout_5",
     platform="qw11q",
     targets=[target],
     update=True,
@@ -13,11 +13,11 @@ with Executor.open(
     e.platform.settings.nshots = 2048
 
     resonator_punchout = e.resonator_punchout(
-        amplitude = 0.1,
+        amplitude = 0.05,
         freq_step = 200000,
-        freq_width = 40000000,
+        freq_width = 80000000,
         max_amp_factor = 2,
-        min_amp_factor = 0,
+        min_amp_factor = 0.1,
         relaxation_time = 5000,
         step_amp_factor = 0.01,    
 )
