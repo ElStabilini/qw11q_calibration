650532c60dcbd01acc55193fbe7a73e1de0dd979
ElStabilini <el.stabilini@gmail.com>
2024-10-04 13:00:45 +0400

    qubit spectroscopy

--- manual_cal/D3_mask/qubit_spectroscopy.py
+++ manual_cal/D3_mask/qubit_spectroscopy.py
@@ -4,7 +4,7 @@ from qibocal.cli.report import report
 target = "D3" 
 with Executor.open(
     "myexec",
-    path="qubit_spectroscopy_1",
+    path="qubit_spectroscopy_2",
     platform="qw11q",
     targets=[target],
     update=True,
@@ -13,10 +13,10 @@ with Executor.open(
     e.platform.settings.nshots = 1024
 
     qubit_spectroscopy = e.qubit_spectroscopy(
-        drive_amplitude = 0.02,
+        drive_amplitude = 0.04,
         drive_duration = 4000,
-        freq_step = 40000,
-        freq_width = 8000000,
+        freq_step = 50000,
+        freq_width = 10000000,
         relaxation_time = 5000,
     )
 
