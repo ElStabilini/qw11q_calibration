c35e2b57a0ccc9754633857041316b5ad650368a
ElStabilini <el.stabilini@gmail.com>
2024-10-04 12:21:21 +0400

    resonator punchout 3

--- manual_cal/D3_mask/resonator_punchout.py
+++ manual_cal/D3_mask/resonator_punchout.py
@@ -4,7 +4,7 @@ from qibocal.cli.report import report
 target = "D3" 
 with Executor.open(
     "myexec",
-    path="resonator_punchout_2",
+    path="resonator_punchout_3",
     platform="qw11q",
     targets=[target],
     update=True,
@@ -13,11 +13,11 @@ with Executor.open(
     e.platform.settings.nshots = 2048
 
     resonator_punchout = e.resonator_punchout(
-        amplitude = 0.01,
+        amplitude = 0.1,
         freq_step = 100000,
         freq_width = 8000000,
         max_amp_factor = 2,
-        min_amp_factor = 0,
+        min_amp_factor = 0.01,
         relaxation_time = 5000,
         step_amp_factor = 0.01,    
 )
