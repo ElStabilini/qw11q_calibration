c36dbb0c960734b43b36fb289f2704b669fb3e4d
ElStabilini <el.stabilini@gmail.com>
2024-10-07 09:29:01 +0200

    reinforce script and reorganization of path

--- rb_optimization.py
+++ rb_optimization.py
@@ -14,24 +14,34 @@ from qibolab.pulses import PulseSequence
 from qibolab.qubits import QubitId
 from scipy.optimize import curve_fit
 
+from qibocal.auto.execute import Executor
 from qibocal import update
 from qibocal.auto.operation import Data, Parameters, Results, Routine
 from qibocal.config import log
-from qibocal.protocols.drag import _fit
-
-'''from qibocal.auto.execute import Executor
-from qibocal.cli.report import report
-from scipy.optimize import least_squares #semplicemente come prova
+from qibocal.protocols.drag import _acquisition
 
 target = "D1"
-with Executor.open(
+platform = "qw11q"
+#devo chiamare l'esecutore per far funzionare la routine?
+#immagino di non aver bisogno di chiamare l'esecutore però devo controllare di passare correttamente gli argomenti (controllare tipi)
+#NB: devo definire una funzione ch prenda come argomento l'esecutore
+
+def test_rb_optimization(executor, _acquisition,
+    #drag_output = e.drag_tuning(beta_start=-4, beta_end=4, beta_step=0.5)
+         
+):
+    
+    return None
+
+target = "D1"
+executor = Executor(
     "myexec",
-    path="test_rx_calibration",
-    platform="qw11q",
+    path="test_rb",
+    platform=platform,
     targets=[target],
     update=True,
     force=True,
-) as e:
-    e.platform.settings.nshots = 2000
+) 
+  
+executor.platform.settings.nshots = 2000
 
-    drag_output = e.drag_tuning(beta_start=-4, beta_end=4, beta_step=0.5)'''
