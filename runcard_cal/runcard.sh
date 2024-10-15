6be8bd2d5efb663bb271ac33887d8530d6a64ffc
ElStabilini <el.stabilini@gmail.com>
2024-10-15 14:38:03 +0400

    resonator spectroscopy high for D1 recalibration

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=resonator_high_s21  # Job name
+#SBATCH --job-name=resonator_high  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/classification.yaml -o /home/users/elisa.stabilini/cal_qw11q/manual_cal/D2_mask/classification_1
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/resonator_spectroscopy_high.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/resonator_spectroscopy_high.yaml
+
