01f1ec4b0e9cdb6107638747a687ad3b563f953d
ElStabilini <el.stabilini@gmail.com>
2024-10-15 19:27:54 +0400

    resonator flux dependence

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,6 +6,6 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/rabi
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/resonator_flux_dependence.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/resonator_flux_dependence
 
 
