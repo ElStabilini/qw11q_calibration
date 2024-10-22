a087a7772769a19e7e1176417de76a8892e5a17f
ElStabilini <el.stabilini@gmail.com>
2024-10-22 17:54:58 +0400

    ramsey with qubit_flux_7 and correct ferquency

--- ramsey_test/runcard.sh
+++ ramsey_test/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/ramsey_test/ramsey.yaml -o /home/users/elisa.stabilini/cal_qw11q/ramsey_test/reportD1/ramsey_3
+qq auto /home/users/elisa.stabilini/cal_qw11q/ramsey_test/ramsey.yaml -o /home/users/elisa.stabilini/cal_qw11q/ramsey_test/reportD1/ramsey_4
