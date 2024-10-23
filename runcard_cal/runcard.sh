48965f687a8a5535ae5fe53c089a744058844569
ElStabilini <el.stabilini@gmail.com>
2024-10-23 12:08:50 +0400

    drag puls calibration

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=t2  # Job name
+#SBATCH --job-name=drag  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/t2.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/t2
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/drag.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/drag
