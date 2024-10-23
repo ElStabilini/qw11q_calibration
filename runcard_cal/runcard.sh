3391d22dcddd05a249b3ef4831f428d4d1cadc43
ElStabilini <el.stabilini@gmail.com>
2024-10-23 13:09:01 +0400

    allxy

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=readout_optimization  # Job name
+#SBATCH --job-name=allxy  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/readout_optimization.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/readout_optimization
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/allxy.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/allxy
