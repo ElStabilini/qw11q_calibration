0f9c45b9b89e2a7b979f11490276dfa95d89a28d
ElStabilini <el.stabilini@gmail.com>
2024-10-23 13:31:05 +0400

    first randomized benchmarking after manual recalibration - no readout charachterization yet

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/allxy.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/allxy
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rb.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/rb
