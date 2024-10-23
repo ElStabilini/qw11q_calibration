990392506f8e5a81b086c4d86f29d38373e66da4
ElStabilini <el.stabilini@gmail.com>
2024-10-23 09:52:30 +0400

    first ramsey test after correct classification

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/ramsey.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/ramsey_2
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/ramsey.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/ramsey_true
