e986950cfe63e6d6830fa6bcadfecdb5600c5b24
ElStabilini <el.stabilini@gmail.com>
2024-10-23 11:36:36 +0400

    measured t1

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=classification  # Job name
+#SBATCH --job-name=t1  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/flipping.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/flipping
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/t1.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/t1
