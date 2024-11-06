532d543e0af26bdb26247de59bc4f8d7d4375ed6
ElStabilini <el.stabilini@gmail.com>
2024-11-06 20:38:33 +0400

    new recalibration folder creation

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=allxy  # Job name
+#SBATCH --job-name=res_spec  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/readout_characterization.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_for_opt/readout_characterization
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/resonator_spectroscopy_high.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/resonator_spectroscopy_high
