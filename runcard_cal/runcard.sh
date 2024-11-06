2bf8e21b4b9f4ed249af9b4a0cdbb056ac01b035
ElStabilini <el.stabilini@gmail.com>
2024-11-06 22:03:06 +0400

    qubit power spectroscopy

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/qubit_spectroscopy.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/qubit_spectroscopy_1
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/qubit_power_spectroscopy.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/qubit_power_spectroscopy
