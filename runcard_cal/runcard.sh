7b62fcdf80b55e5e72fd37ad427d89d9784cc214
ElStabilini <el.stabilini@gmail.com>
2024-11-07 00:25:25 +0400

    flipping

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/ramsey.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/ramsey
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/flipping.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/flipping
