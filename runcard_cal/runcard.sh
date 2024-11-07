336da2fef44ef2b4c349f5b1a266badbd72712ab
ElStabilini <el.stabilini@gmail.com>
2024-11-07 21:21:56 +0400

    recalibration 2

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/classification.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/classification_3
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalibration.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/recalibration_2
