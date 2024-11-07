37e0beb3e3ff75294cd026b2a0a593808e31ba34
ElStabilini <el.stabilini@gmail.com>
2024-11-07 19:08:44 +0400

    performing again resonator spectroscopy

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=ramsey  # Job name
+#SBATCH --job-name=qq  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude_frequency.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/rabi_1
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/resonator_spectroscopy_low.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/resonator_spectroscopy_low_2
