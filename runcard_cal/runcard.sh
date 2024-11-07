e66c2d8c77ac20e5c24147ca2d1f040c6afe2cf7
ElStabilini <el.stabilini@gmail.com>
2024-11-07 19:04:07 +0400

    rerun ramsey

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/rabi_1
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude_frequency.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/rabi_1
