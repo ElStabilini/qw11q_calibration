b18f9129e99073db5a2cc980546f4d96091eb7f2
ElStabilini <el.stabilini@gmail.com>
2024-11-07 14:07:50 +0400

    ramsey again

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=res_spec  # Job name
+#SBATCH --job-name=ramsey  # Job name
 #SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/classification.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/classification_2
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/ramsey.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/ramsey_1
