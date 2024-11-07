33211c71d04d8e938f52104e54dad845361e5725
ElStabilini <el.stabilini@gmail.com>
2024-11-08 00:24:36 +0400

    rabi amplitude

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/half_calibration.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recal_071124/half_calibration
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recal_071124/rabi_amplitude
