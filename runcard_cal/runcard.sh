b2212dabd9ab3d013efc8a8060a98399c1e414d8
ElStabilini <el.stabilini@gmail.com>
2024-11-08 13:19:58 +0400

    resonator spectroscopy low

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/resonator_punchout.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/secondtry/resonator_punchout
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/resonator_spectroscopy_low.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/recalD1_061124/secondtry/resonator_spectroscopy_low
