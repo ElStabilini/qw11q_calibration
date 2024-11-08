7a163c0f5ec3e90266d345582a9cb5a297e013f3
Elisa Stabilini <el.stabilini@gmail.com>
2024-11-08 08:14:12 +0100

    Delete manual_cal/tests directory

--- manual_cal/tests/manual.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=manual_res  # Job name
-#SBATCH --time=01:00:00            # Time limit
-#SBATCH --partition=qw11q            # Partition name
-
-#run the script
-python manual_resonator_s21.py 
