2d68e428294ac466cdeaadc31b41a58818927b26
ElStabilini <el.stabilini@gmail.com>
2024-11-10 19:14:46 +0400

    performed drag post ramsey, flipping and classification to check \beta parameter convergence

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/classification.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/ft_091124/classification
+qq auto /home/users/elisa.stabilini/cal_qw11q/runcard_cal/drag.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/ft_091124/drag
