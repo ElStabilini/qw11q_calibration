04c4120234ed21c06f41363357a0376096f42937
ElStabilini <el.stabilini@gmail.com>
2024-11-20 12:07:26 +0400

    pi half amplitude calibration with more points

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,5 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_pihalf/rabi_pi_pihb
+qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_pihalf/rabi_p
+ihalf_dense
