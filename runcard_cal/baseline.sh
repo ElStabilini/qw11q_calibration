94c5da77a29c196a45a07fb7af67aec3a04350ad
ElStabilini <el.stabilini@gmail.com>
2024-11-20 12:51:12 +0400

    pi rabi amplitude frequency on branch 02

--- runcard_cal/baseline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# SBATCH directives (optional)
-#SBATCH --job-name=qq  # Job name
-#SBATCH --time=01:00:00            # Time limit
-#SBATCH --partition=qw11q            # Partition name
-
-# Run the qq acquire command
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_pihalf/rabi_pi02
--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q            # Partition name
 
 # Run the qq acquire command
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_pihalf/rab_pihalf_dense
+qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_amplitude_frequency.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_af_pihalf/rabi_02
