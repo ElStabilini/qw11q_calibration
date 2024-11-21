9e620c6f9db7f5659023f400ef5d76c0f22ee139
ElStabilini <el.stabilini@gmail.com>
2024-11-21 16:55:37 +0400

    resonator_punchout

--- runcard_cal/qua_script.py
+++ runcard_cal/qua_script.py
@@ -1,5 +1,5 @@
 
-# Single QUA script generated at 2024-11-21 12:08:56.229963
+# Single QUA script generated at 2024-11-21 12:38:03.175896
 # QUA library version: 1.1.6
 
 from qm.qua import *
@@ -8,26 +8,28 @@ with program() as prog:
     v1 = declare(int, )
     v2 = declare(fixed, )
     v3 = declare(fixed, )
-    v4 = declare(int, )
+    v4 = declare(fixed, )
+    v5 = declare(int, )
     set_dc_offset("D1/flux", "single", 0.2205)
     set_dc_offset("D2/flux", "single", -0.421)
     set_dc_offset("D3/flux", "single", -0.2095)
     set_dc_offset("D4/flux", "single", 0.0)
     set_dc_offset("D5/flux", "single", -0.04)
     wait((4+(0*(Cast.to_int(v2)+Cast.to_int(v3)))), "D1/acquisition")
-    with for_(v1,0,(v1<1024),(v1+1)):
-        with for_(v4,-319593007,(v4<=-311643007),(v4+50000)):
-            update_frequency("D1/acquisition", v4, "Hz", False)
-            align()
-            measure("4742235378630614813", "D1/acquisition", None, dual_demod.full("cos", "out1", "sin", "out2", v2), dual_demod.full("minus_sin", "out1", "cos", "out2", v3))
-            r1 = declare_stream()
-            save(v2, r1)
-            r2 = declare_stream()
-            save(v3, r2)
-            wait(1250, )
+    with for_(v1,0,(v1<2048),(v1+1)):
+        with for_(v4,0.10473684210526318,(v4<2.094736842105263),(v4+0.2094736842105263)):
+            with for_(v5,-314902686,(v5<=-310002686),(v5+100000)):
+                update_frequency("D1/acquisition", v5, "Hz", False)
+                align()
+                measure("2252675425701575006"*amp(v4), "D1/acquisition", None, dual_demod.full("cos", "out1", "sin", "out2", v2), dual_demod.full("minus_sin", "out1", "cos", "out2", v3))
+                r1 = declare_stream()
+                save(v2, r1)
+                r2 = declare_stream()
+                save(v3, r2)
+                wait(1250, )
     with stream_processing():
-        r1.buffer(160).buffer(1024).save("4742235378630614813_D1|acquisition_I")
-        r2.buffer(160).buffer(1024).save("4742235378630614813_D1|acquisition_Q")
+        r1.buffer(50).buffer(10).average().save("2252675425701575006_D1|acquisition_I")
+        r2.buffer(50).buffer(10).average().save("2252675425701575006_D1|acquisition_Q")
 
 
 config = {
@@ -174,20 +176,20 @@ config = {
                     "buffer": 18,
                 },
             },
-            "intermediate_frequency": -315593006.56694794,
+            "intermediate_frequency": -312402685.6579981,
             "time_of_flight": 224.0,
             "smearing": 0.0,
             "operations": {
-                "4742235378630614813": "4742235378630614813_D1/acquisition",
+                "2252675425701575006": "2252675425701575006_D1/acquisition",
             },
         },
     },
     "pulses": {
-        "4742235378630614813_D1/acquisition": {
+        "2252675425701575006_D1/acquisition": {
             "length": 2000.0,
             "waveforms": {
-                "I": "5322797260786059624_i",
-                "Q": "5322797260786059624_q",
+                "I": "-5688410571078471639_i",
+                "Q": "-5688410571078471639_q",
             },
             "digital_marker": "ON",
             "operation": "measurement",
@@ -199,11 +201,11 @@ config = {
         },
     },
     "waveforms": {
-        "5322797260786059624_i": {
+        "-5688410571078471639_i": {
             "sample": 0.0005,
             "type": "constant",
         },
-        "5322797260786059624_q": {
+        "-5688410571078471639_q": {
             "sample": 0.0,
             "type": "constant",
         },
@@ -394,24 +396,24 @@ loaded_config = {
             },
             "time_of_flight": 224,
             "smearing": 0,
-            "intermediate_frequency": -315593006.56694794,
+            "intermediate_frequency": -312402685.6579981,
             "operations": {
-                "4742235378630614813": "4742235378630614813_D1/acquisition",
+                "2252675425701575006": "2252675425701575006_D1/acquisition",
             },
             "mixInputs": {
                 "I": ('con6', 1),
                 "Q": ('con6', 2),
-                "mixer": "D1/acquisition_mixer_63c",
+                "mixer": "D1/acquisition_mixer_aa2",
                 "lo_frequency": 7450000000.0,
             },
         },
     },
     "pulses": {
-        "4742235378630614813_D1/acquisition": {
+        "2252675425701575006_D1/acquisition": {
             "length": 2000,
             "waveforms": {
-                "I": "5322797260786059624_i",
-                "Q": "5322797260786059624_q",
+                "I": "-5688410571078471639_i",
+                "Q": "-5688410571078471639_q",
             },
             "digital_marker": "ON",
             "integration_weights": {
@@ -423,11 +425,11 @@ loaded_config = {
         },
     },
     "waveforms": {
-        "5322797260786059624_i": {
+        "-5688410571078471639_i": {
             "sample": 0.0005,
             "type": "constant",
         },
-        "5322797260786059624_q": {
+        "-5688410571078471639_q": {
             "sample": 0.0,
             "type": "constant",
         },
@@ -452,7 +454,7 @@ loaded_config = {
         },
     },
     "mixers": {
-        "D1/acquisition_mixer_63c": [{'intermediate_frequency': -315593006.56694794, 'lo_frequency': 7450000000.0, 'correction': [1, 0.0, 0.0, 1]}],
+        "D1/acquisition_mixer_aa2": [{'intermediate_frequency': -312402685.6579981, 'lo_frequency': 7450000000.0, 'correction': [1, 0.0, 0.0, 1]}],
     },
 }
 
