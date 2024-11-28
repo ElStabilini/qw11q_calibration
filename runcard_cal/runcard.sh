18b0823d5bbd01c43e2d0a2d2a5f5740ed2d1b4c
ElStabilini <el.stabilini@gmail.com>
2024-11-28 12:53:49 +0400

    rabi length signal and rabi length

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q          # Partition name
 
 # Run the qq acquire command
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_length_signal.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/RX90/flip/rx/rabi_length_signal
+qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_length.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/RX90/flip/rx/rabi_length
