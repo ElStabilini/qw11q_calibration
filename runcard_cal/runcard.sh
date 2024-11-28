58baea305b2247999d42ba9e9592d080de89a876
ElStabilini <el.stabilini@gmail.com>
2024-11-28 18:19:42 +0400

    rx90flipping

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=qw11q          # Partition name
 
 # Run the qq acquire command
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/flipping.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/RX90/flip/rx/flipping
+qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/flipping.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/RX90/flip/rx90/flipping
