9c060de73c6c44ce6d935979d5612daaadc617b0
ElStabilini <el.stabilini@gmail.com>
2025-01-06 20:10:33 +0400

    first classification test before running cryoscope

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -3,7 +3,10 @@
 # SBATCH directives (optional)
 #SBATCH --job-name=qq  # Job name
 #SBATCH --time=01:00:00            # Time limit
-#SBATCH --partition=qw11q            # Partition name
+#SBATCH --partition=qw11q          # Partition name
 
 # Run the qq acquire command
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_length.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/rabi_length_pihalf/rabi_length
+
+current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+
+qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/classification.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/cryoscope_tests/postprocessing/classification_${current_time}
