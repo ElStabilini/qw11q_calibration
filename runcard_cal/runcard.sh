9782bd134edd8d36120d316017b9259c91e98ba4
ElStabilini <el.stabilini@gmail.com>
2025-02-04 18:47:41 +0400

    B classification

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -2,12 +2,12 @@
 
 # SBATCH directives (optional)
 #SBATCH --job-name=qq  # Job name
-#SBATCH --time=01:00:00            # Time limit
+#SBATCH --time=022:00:00            # Time limit
 #SBATCH --partition=qw11q          # Partition name
 
 # Run the qq acquire command
 
-routine="cryoscope_amplitude"
+routine="classification"
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 
 qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${routine}.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/cryoscope_tests/postprocessing/Bline/${routine}_${current_time}
