5469c92120c5b39028e3e510a4753c9b1937e927
ElStabilini <el.stabilini@gmail.com>
2025-02-09 11:30:20 +0400

    fix folders name

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -11,4 +11,4 @@ routine="classification"
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 folder="0.1"
 
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${routine}.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/folder/${routine}_${current_time}
+qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${routine}.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${folder}/${routine}_${current_time}
