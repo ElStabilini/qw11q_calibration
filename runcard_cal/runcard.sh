648ff5642e135563a0267b8dae72c487d9e92599
ElStabilini <el.stabilini@gmail.com>
2025-02-28 13:22:26 +0400

    cryoscope

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -10,7 +10,7 @@
 export QIBOLAB_PLATFORMS=~/qibolab_platforms_qrc
 source /home/users/elisa.stabilini/calibration/bin/activate
 
-routine="cryoscope_amplitude"
+routine="cryoscope"
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 folder="qibocal_cryo"
 
