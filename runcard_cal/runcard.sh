0b75845cbb3e95a19719b69669ac1041e6853ff9
ElStabilini <el.stabilini@gmail.com>
2025-03-12 17:41:28 +0400

    test status

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -14,4 +14,4 @@ routine="classification"
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 folder="test_dependencies"
 
-qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${routine}.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${folder}/${routine}_${current_time}
+qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${routine}.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${routine}_${current_time}
