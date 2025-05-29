02e35f6a0f0e69635a2c3d3f07bb1df7a11c1d70
ElStabilini <el.stabilini@gmail.com>
2025-05-29 15:51:27 +0000

    test classification

--- runcard_cal/runcard.sh
+++ runcard_cal/runcard.sh
@@ -3,15 +3,15 @@
 # SBATCH directives (optional)
 #SBATCH --job-name=qq  # Job name
 #SBATCH --time=20:00:00            # Time limit
-#SBATCH --partition=qw11q          # Partition name
+#SBATCH --partition=qw5q_platinum          # Partition name
 
 # Run the qq acquire command
 
 export QIBOLAB_PLATFORMS=~/qibolab_platforms_qrc
 source /home/users/elisa.stabilini/calibration/bin/activate
 
-routine="rabi_amplitude_frequency"
+routine="classification"
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
-folder="qw5q"
+folder="replica"
 
 qq run /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${routine}.yaml -o /home/users/elisa.stabilini/cal_qw11q/runcard_cal/${folder}/${routine}_${current_time}
